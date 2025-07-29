4261d3019c2ea875f843cdc59a71ff964dab0777
Aline Abler <aline.abler@vshn.ch>
2025-07-29 10:56:28 +0200

    Add basic authentication

--- pkg/api/apiserver.go
+++ pkg/api/apiserver.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"crypto/sha256"
+	"crypto/subtle"
 	"fmt"
 	"net/http"
 
@@ -33,5 +35,28 @@ func NewApiServer(config ApiServerConfig, store *store.DowntimeStore) ApiServer 
 
 func (s *ApiServer) Start() error {
 	var hostport = fmt.Sprintf("%s:%d", s.config.Host, s.config.Port)
-	return http.ListenAndServe(hostport, s.mux)
+	return http.ListenAndServe(hostport, s.basicAuth(s.mux))
+}
+
+
+func (s *ApiServer) basicAuth(next http.Handler) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		username, password, ok := r.BasicAuth()
+		if ok {
+			usernameHash := sha256.Sum256([]byte(username))
+			passwordHash := sha256.Sum256([]byte(password))
+			expectedUsernameHash := sha256.Sum256([]byte(s.config.AuthUser))
+			expectedPasswordHash := sha256.Sum256([]byte(s.config.AuthPass))
+
+			usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			if usernameMatch && passwordMatch {
+				next.ServeHTTP(w, r)
+				return
+			}
+		}
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+	})
 }
