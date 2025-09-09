d91dc490ded9f0bd86e81f4fb75ba74aef245de9
Sebastian Widmer <sebastian.widmer@vshn.net>
2025-09-09 20:51:10 +0200

    Remove unused variables

--- main.go
+++ main.go
@@ -1,19 +1,7 @@
 package main
 
 import (
-	"time"
-
 	"github.com/vshn/vshn-sli-reporting/pkg/cmd"
-)
-
-var (
-	// these variables are populated by Goreleaser when releasing
-	version = "unknown"
-	commit  = "-dirty-"
-	date    = time.Now().Format("2006-01-02")
-
-	appName     = "vshn-sli-reporting"
-	appLongName = "VSHN SLI Reporting"
 )
 
 func main() {
